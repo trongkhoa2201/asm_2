82b017572adc82fca8991101b472e4725bf90be6
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2021-06-23 12:26:53 +0700

    Update update1.php

--- update1.php
+++ update1.php
@@ -1,4 +1,4 @@
-<title>UPDATE DATABASE</title>
+<html>
 <?php
                         $host_heroku = "ec2-54-91-188-254.compute-1.amazonaws.com";
 			$db_heroku = "d3avp12ob9g5o8";
@@ -11,39 +11,71 @@
 			$result = pg_query($pg_conn, "select * from product;");
 			#var_dump(pg_fetch_all($result));
 			
-			$numrows = pg_num_rows($result)
-	$result = pg_Exec($conn,"SELECT * FROM product WHERE product_id, product_name, price, amount='$shop'");
-	if (!$result) {echo "A query error occurred.\n"; exit;}
-	$product_id = pg_Result($result, $shop, "product_id");
-	$product_name = pg_Result($result, $shop, "product_name");
-	$price = pg_Result($result, $shop, "price");
-	$amount = pg_Result($result, $shop, "amount");
-	pg_FreeResult($result);
-	pg_Close($conn);
 if (!$pg_conn)
 {
 die('Error: Could not connect: ' . pg_last_error());
 }
-
+$product_id=$_GET['product_id'];
+$product_name=$_GET['n'];
+$price=$_GET['price'];
+$amount=$_GET['amount'];
 ?>
-<html>
-<body>
-  <b>Please update the following:</b></font>
-  <p><font size="2" face="Arial, Helvetica, sans-serif">
-  <form action="edit.php?ID=<? echo $CID ?>" method="POST" enablecab="Yes">
-  product_id:<br>
-  <input type="Text" name="product_id" align="LEFT" required="Yes" size="59" 
-  value="<? echo $CName ?>"><br>
-  product_name:<br>
-  <input type="Text" name="product_name" align="LEFT" required="Yes" size="59"
-  value="<? echo $CAddress ?>"><br>
-  price:<br>
-  <input type="Text" name="price" align="LEFT" required="Yes" size="29"
-  value="<? echo $CCity ?>"><br>
-  amount:<br>
-  <input type="Text" name="amount" align="LEFT" required="Yes" size="2"
-  value="<? echo $CState ?>"><br>
-  </form>
+ <head>
+ <title> Update </title>
+ </head>
+ <body>
+
+ <br>
+ <form action="" method="GET">
+
+
+ <tr>
+ <td>Product ID</td>
+ <td><input type="text" value="<?php echo "$product_id" ?>" name="product_id" required></td>
+ </tr>
+
+ <tr>
+ <td>Product Name</td>
+ <td><input type="text" value="<?php echo "$product_name" ?>" name="product_name" required></td>
+ </tr>
+
+ <tr>
+ <td>Product Price</td>
+ <td><input type="text" value="<?php echo "$price" ?>" name="price" required></td>
+ </tr>
+
+ <tr>
+ <td>Amount</td>
+ <td><input type="text" value="<?php echo "$amount" ?>" name="amount" required></td>
+ </tr>
+
+ <tr>
+ <td colspan="2" align="center"><input type="submit" id="button" name="submit" value="Update"></td>
+ </tr>
+ </form>
+ </table>
 </body>
 </html>
+<?php
+if($_GET['submit'])
+{
+$productid = $_GET['product_id'];
+$productname = $_GET['product_name'];
+$productprice = $_GET['price'];
+$status = $_GET['amount'];
+  $query = "UPDATE shop1 SET productid='$product_id', productname='$product_name', productprice='$price', status='$amount' WHERE productid='$product_id' ";
+$data = pg_query($pg_conn,$query);
+if($data)
+{
+echo "<script>alert('Updated Successfully!')</script>";
+?>
+<meta http-equiv="refresh" content="0; url=https://cloudasm-2.herokuapp.com/db_selection.php">
+<?php
+}
+else
+{
+echo "Failed to update the table.";
+}
+}
+?>
 
