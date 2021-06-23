4a78629b9ef115f6655d05efa0ffa1715e9c7e2c
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2021-06-23 12:26:32 +0700

    Add files via upload

---
+++ updaten.php
@@ -0,0 +1,42 @@
+<html>
+
+ <head>
+ <title> Update </title>
+ </head>
+ <body>
+
+ <br>
+ <form action="" method="GET">
+
+
+ <tr>
+ <td>Product ID</td>
+ <td><input type="text" value="x" name="product_id" required></td>
+ </tr>
+
+ <tr>
+ <td>Product Name</td>
+ <td><input type="text" value="x" name="product_name" required></td>
+ </tr>
+
+ <tr>
+ <td>Product Price</td>
+ <td><input type="text" value="x" name="price" required></td>
+ </tr>
+
+ <tr>
+ <td>Amount</td>
+ <td><input type="text" value="x" name="amount" required></td>
+ </tr>
+
+ <tr>
+ <td colspan="2" align="center"><input type="submit" id="button" name="submit" value="Update"></td>
+ </tr>
+ </form>
+ </table>
+</body>
+</html>
+
+
+
+
