3158bf54be5ac0ce8d6d5f70021b62120cb1f55d
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2021-06-24 12:38:06 +0700

    Add files via upload

---
+++ db_selection.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title> ATN shop page </title>
+</head>
+<h1> PAGE FOR SHOP <h1/>
+
+<body>
+	<form action="" method="post">
+        <input type="submit" name="deleteButton"
+                value="Input delete data"/> <br/><br/>
+        <input type="submit" name="updateButton"
+                value="Input update data"/> <br/><br/>
+          
+    </form>
+	<?php
+		session_start();
+		$shop = $_SESSION["shop"];
+        if(isset($_POST['updateButton'])) 
+		{
+            header("location: updaten.php");
+        }
+        if(isset($_POST['deleteButton'])) 
+		{
+            header("location: updaten.php");
+        }
+        
+    ?>
+
+	<?php
+		function exceptions_error_handler($severity, $message, $filename, $lineno) 
+		{
+			throw new ErrorException($message, 0, $severity, $filename, $lineno);
+		}
+
+		set_error_handler('exceptions_error_handler');
+		$input = "Shop_A";
+		$table_name = "product";
+		include("config.php");
+		include("db_display.php");
+		//check if form was submitted
+		if(isset($_POST['submitButton'])) 
+		{ 
+			//get input text
+			$input = $_POST['db_prob'];
+		}
+		# Try to display SQL table
+		try 
+		{
+			echo "<p> DATABASE FROM ".$input."</p>"; 
+			
+			$pg_heroku = pg_connect($conn_string);
+											
+			if ($input == "Shop_A")
+			{
+				# Get data by query
+				$result = pg_query($pg_heroku, "select * from ".$table_name." where shop_name='$input'");
+			}
+			display_table($result);
+			pg_close();
+		}
+		catch (Exception $e) 
+		{
+			#echo 'Caught exception: ',  $e->getMessage(), "\n";
+			echo "Caught exception: <br/>", $e->getMessage(), "\n";
+		}
+	?>
+	<br/>
+	<?php
+	if($_SESSION["name"]) {
+	?>
+	Click here to <a href="logout.php" tite="Logout">Logout.
+	<?php
+	}else echo "<h1>Please login first .</h1>";
+	?>
+</body>
+</html>
