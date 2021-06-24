b463b9f6a7c8c913bd48d55fa5ae791eabba8dc1
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2021-06-24 15:56:15 +0700

    Update db_display.php

--- db_display.php
+++ db_display.php
@@ -1,56 +1,83 @@
- <?php
-# argument: query results
-	function display_table($query_obj)
-	{
-		$num_rows = pg_num_rows($query_obj);
-		$num_fields = pg_num_fields($query_obj);
-		# Init a table
-		echo ' <table border="1">
-			   <tr>';
-	    $field_list = array();
-		# Diplay header
-		for ($i=0; $i<$num_fields; $i++)
-		{
-			$field_name = pg_field_name($query_obj, $i);
-			$field_list[$i] = $field_name;
-			echo "<th> $field_name </th>";
-		}
-		echo "</tr>";
-		# diplay a row function
-		function display_row($row, $fieldlist)
-		{
-			echo "<tr>\n";
-			foreach ($fieldlist as $fieldname)
-			{
-				echo "<td>", $row[$fieldname], "</td>";
-			}
-			echo "</tr>";			
-		}
-		# display all rows
-		for ($row_index=0; $row_index<$num_rows; $row_index++)
-		{
-			$row = pg_fetch_array($query_obj, $row_index);
-			# display a row
-			display_row($row, $field_list);
-		}
-		echo "</table>";
-	}
-?>
-<?php
-		
+<html>
+<head>
+<meta charset="UTF-8">
+<title> ATN shop page </title>
+</head>
+<h1> PAGE FOR SHOP <h1/>
 
-			# DATABASE credential
-			$host_heroku = "ec2-54-91-188-254.compute-1.amazonaws.com";
-			$db_heroku = "d3avp12ob9g5o8";
-			$user_heroku = "vgvukkjgszbaba";
-			$pw_heroku = "6c4e536add531cd9f19cf9056e038d4919f1cd1eab35d78862f9d22525e61ee9";
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			# Connect to DATABASE
-			$pg_conn = pg_connect($conn_string);
-			# Get data by query
-			$result = pg_query($pg_conn, "select * from productb;");
-			#var_dump(pg_fetch_all($result));
+<body>
+	<form action="" method="post">
+        <input type="submit" name="deleteButton"
+                value="Input delete data"/> <br/><br/>
+        <input type="submit" name="updateButton"
+                value="Input update data"/> <br/><br/>
+          
+    </form>
+	<?php
+		session_start();
+		$shop = $_SESSION["shop"];
+        if(isset($_POST['updateButton'])) 
+		{
+			$shop_name = $_GET['shop_name'];
+			$product_id = $_GET['product_id'];
+			$product_name = $_GET['product_name'];
+			$price = $_GET['price'];
+			$amount = $_GET['amount'];
+            header("location: update1.php");
+
+        }
+        if(isset($_POST['deleteButton'])) 
+		{
+            header("location: delete1.php");
+        }
+        
+    ?>
+
+	<?php
+		function exceptions_error_handler($severity, $message, $filename, $lineno) 
+		{
+			throw new ErrorException($message, 0, $severity, $filename, $lineno);
+		}
+
+		set_error_handler('exceptions_error_handler');
+		$input = "Shop_A";
+		$table_name = "product";
+		include("config.php");
+		include("db_display.php");
+		//check if form was submitted
+		if(isset($_POST['submitButton'])) 
+		{ 
+			//get input text
+			$input = $_POST['db_prob'];
+		}
+		# Try to display SQL table
+		try 
+		{
+			echo "<p> DATABASE FROM ".$input."</p>"; 
 			
-			$numrows = pg_num_rows($result)
-		?>
+			$pg_heroku = pg_connect($conn_string);
+											
+			if ($input == "Shop_A")
+			{
+				# Get data by query
+				$result = pg_query($pg_heroku, "select * from ".$table_name." where shop_name='$input'");
+			}
+			display_table($result);
+			pg_close();
+		}
+		catch (Exception $e) 
+		{
+			#echo 'Caught exception: ',  $e->getMessage(), "\n";
+			echo "Caught exception: <br/>", $e->getMessage(), "\n";
+		}
+	?>
+	<br/>
+	<?php
+	if($_SESSION["name"]) {
+	?>
+	Click here to <a href="logout.php" tite="Logout">Logout.
+	<?php
+	}else echo "<h1>Please login first .</h1>";
+	?>
+</body>
+</html>
